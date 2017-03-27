cec46f672caafc43afe3dc2ceb716aa195733eb4
Lucy Wang <LucyTheGreat@Lucys-MacBook-Air.local>
2017-03-27 12:00:03 -0400

    server logic basic

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 
-const amazon      = require('amazon-product-api');
+const amazon = require('amazon-product-api');
 const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
@@ -18,6 +18,14 @@ const knexLogger  = require('knex-logger');
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 const login = require("./routes/login");
+
+const client = amazon.createClient({
+  awsId: process.env.awsId,
+  awsSecret:process.env.awsSecret,
+  awsTag:process.env.awsTag
+});
+
+// const search = require('./routes/search');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -40,39 +48,53 @@ app.use(express.static("public"));
 // Mount all resource routes
 app.use("/register", usersRoutes(knex));
 app.use('/login', login(knex));
+// app.use('/search',search);
 // Home page
 app.get("/", (req, res) => {
+
   res.render("index");
 });
 
-const client = amazon.createClient({
-  awsId: process.env.AWS_ID,
-  awsSecret: process.env.AWS_SECRET,
-  awsTag: process.env.AWS_TAG
+app.get('/search', (req,res) => {
+  res.render('search');
 });
 
-client.itemSearch({
-  brand: 'Samsung',
-  keywords: 'television',
-  title: 'TV',
-  ItemPage: 1,
-  sort: 'salesrank',
-  searchIndex: 'Electronics',
-  condition: 'New',
-  responseGroup: 'ItemAttributes,Images'
-}).then(function(results){
-  app.get("/test", (req, res) => {
-    let templateVars = {
-      image1: results[0].LargeImage[0].URL,
-      brand1: results[0].ItemAttributes[0].Brand,
-      ProductType1: results[0].ItemAttributes[0].ProductTypeName,
-      DetailPageURL1: results[0].DetailPageURL
-        // jsonObj: res.json(results)
-    };
-      res.render("test2", templateVars);
+
+app.post('/search', (req,res) => {
+  client.itemSearch({
+    brand: req.params.brand1,
+    // keywords: 'television',
+    title: req.params.product,
+    ItemPage: 1,
+    sort: 'salesrank',
+    searchIndex: 'Electronics',
+    responseGroup: 'ItemAttributes,Images'
+  }).then(function(results){
+    // app.get("/test", (req, res) => {
+      console.log(results);
+      // let templateVars = {
+      //   image1: results[0].LargeImage[0].URL,
+      //   brand1: results[0].ItemAttributes[0].Brand,
+      //   ProductType1: results[0].ItemAttributes[0].ProductTypeName,
+      //   DetailPageURL1: results[0].DetailPageURL
+      //     // jsonObj: res.json(results)
+      // };
+        // res.render("test2", templateVars);
+    // });
+  }).catch(function(err){
+    console.log(err);
   });
-}).catch(function(err){
-  console.log(err);
+});
+
+app.get("/test", (req, res) => {
+  let templateVars = function(results){
+    image1: results[0].LargeImage[0].URL,
+    brand1: results[0].ItemAttributes[0].Brand,
+    ProductType1: results[0].ItemAttributes[0].ProductTypeName,
+    DetailPageURL1: results[0].DetailPageURL
+      // jsonObj: res.json(results)
+  };
+    res.render("test2", templateVars);
 });
 
 
