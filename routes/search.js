4ec4489bceb5be28072a12b242e8fea29ae8184f
cy <cybc00@hotmail.com>
2017-03-28 09:49:49 -0400

    Now inserts appropriate displayed randomized product to corresponding columns in comparisons table

--- routes/search.js
+++ routes/search.js
@@ -13,7 +13,7 @@ const client = amazon.createClient({
 const rand1 = Math.floor(Math.random() * 5);
 const rand2 = Math.floor(Math.random() * 5);
 
-module.exports = () => {
+module.exports = (knex) => {
   searchRouter.get("/", (req, res) => {
     console.log(req.query.brand1, req.query.brand2);
     Promise.all([
@@ -36,25 +36,43 @@ module.exports = () => {
         responseGroup: 'ItemAttributes,Images'
       })]).
     then(function(results){
-
+      const productTitles = {
+        pro1: results[0][rand1].ItemAttributes[0].Title,
+        pro2: results[1][rand2].ItemAttributes[0].Title
+      };
       console.log(results[0][rand1])
       let templateVars = {
         br1: {
           image1: results[0][rand1].LargeImage[0].URL,
           brand1: results[0][rand1].ItemAttributes[0].Brand,
           ProductType1: results[0][rand1].ItemAttributes[0].ProductTypeName,
-          DetailPageURL1: results[0][rand1].DetailPageURL
+          DetailPageURL1: results[0][rand1].DetailPageURL,
+          pTitle1: results[0][rand1].ItemAttributes[0].Title,
+          description: results[0][rand1].ItemAttributes[0].Feature,
         },
           br2: {
             image2: results[1][rand2].LargeImage[0].URL,
             brand2: results[1][rand2].ItemAttributes[0].Brand,
             ProductType2: results[1][rand2].ItemAttributes[0].ProductTypeName,
-            DetailPageURL2: results[1][rand2].DetailPageURL
+            DetailPageURL2: results[1][rand2].DetailPageURL,
+            pTitle2: results[1][rand2].ItemAttributes[0].Title,
+            description: results[0][rand2].ItemAttributes[0].Feature
           }
           // jsonObj: res.json(results)
       }
-      console.log(rand1, rand2);
+        console.log(rand1, rand2);
         res.render("test2", templateVars);
+        return productTitles;
+    }).then(function(results) {
+        console.log(results);
+        knex.insert({product_one: results.pro1[0], product_two: results.pro2[0]})
+            .into('comparisons')
+            .then(function(results) {
+              return results;
+            })
+            .catch(function(err) {
+              console.log(err);
+            })
     }).catch(function(err){
       console.log('ERROR', err);
     });
