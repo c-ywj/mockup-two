62356042ba5fe98f2bbed984297ea3ce47f77bdf
cy <cybc00@hotmail.com>
2017-03-29 12:01:10 -0400

    vote route handler now checks if pairing exists in database

--- public/scripts/vote.js
+++ public/scripts/vote.js
@@ -6,13 +6,29 @@ $(() => {
     console.log(pro1Title);
     console.log(pro2Title);
 
-    var data = {
-      pro1Title: pro1Title,
-      pro2Title: pro2Title
-    }
-
 
   $('#votePro1').click(function(ev) {
+    const data = {
+      votedPro: pro1Title,
+      unvotedPro: pro2Title
+    };
+    console.log('clicked');
+    ev.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/searchres",
+      data: data
+    }).done(function(msg){
+      console.log('this is the msg: ' + msg);
+      alert('VOTED!');
+    });
+  })
+
+  $('#votePro2').click(function(ev) {
+    const data = {
+      votedPro: pro2Title,
+      unvotedPro: pro1Title
+    };
     console.log('clicked');
     ev.preventDefault();
     $.ajax({
--- routes/vote.js
+++ routes/vote.js
@@ -6,7 +6,17 @@ const voteRouter = express.Router();
 module.exports = (knex) => {
 
   voteRouter.post('/', (req, res) => {
-    console.log(req.body.pro1Title, req.body.pro2Title);
+    console.log('voted pro is: ' +req.body.votedPro, 'unvoted pro is: ' + req.body.unvotedPro);
+    const votedPro   = req.body.votedPro;
+    const unvotedPro = req.body.unvotedPro;
+    knex('comparisons').where({
+      product_one: votedPro,
+      product_two: unvotedPro
+    }).select('*').then(function(result) {
+      console.log(result);
+    }).catch(function(err) {
+      console.log(err);
+    })
   })
 
   return voteRouter;
