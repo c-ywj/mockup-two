cd2b126c048b304e0fc1410d4a8ccf60af5d6d68
cy <cybc00@hotmail.com>
2017-03-29 12:59:23 -0400

    inserts product titles in string format in db; also clean up

--- routes/search.js
+++ routes/search.js
@@ -30,6 +30,7 @@ const amzSearch = function(brand, category) {
 module.exports = (knex) => {
   searchRouter.get("/", (req, res) => {
     console.log(req.query.brand1, req.query.brand2);
+
     Promise.all([
       amzSearch(req.query.brand1, req.query.category),
       amzSearch(req.query.brand2, req.query.category)
@@ -43,49 +44,47 @@ module.exports = (knex) => {
         title: results[rand4][rand2].ItemAttributes[0].Title,
         type:  results[rand4][rand2].ItemAttributes[0].ProductTypeName
       }
-      console.log('this is pro1 :' + pro1.type);
-      console.log('this is pro2 :' + pro2.type);
-      knex.select('*').from('comparisons')
-                      .where('product_one', '=', pro1.title[0],
-                             'product_two', '=', pro2.title[0]
-                             )
+
+      return knex
+        .select('*')
+        .from('comparisons')
+        .where(
+          'product_one', 'LIKE', pro2.title[0],
+          'product_two', 'LIKE', pro1.title[0]
+        )
         .then(function(result) {
-          console.log(result);
-          if(result.length === 0 && pro1.type[0] === pro2.type[0] && pro1.title !== pro2.title) {
-            knex.insert({product_one: pro1.title, product_two: pro2.title}).into('comparisons')
-                .then(function(result) {
-                  // console.log(pro1.type);
-                  // return result;
-                  const productTitles = {
-                      pro1: results[rand3][rand1].ItemAttributes[0].Title,
-                      pro2: results[rand4][rand2].ItemAttributes[0].Title
-                    };
-                  // console.log(results[0][rand1])
-                  let templateVars = {
-                    br1: {
-                      image1: results[rand3][rand1].LargeImage[0].URL,
-                      brand1: results[rand3][rand1].ItemAttributes[0].Brand,
-                      ProductType1: results[rand3][rand1].ItemAttributes[0].ProductTypeName,
-                      DetailPageURL1: results[rand3][rand1].DetailPageURL,
-                      pTitle1: results[rand3][rand1].ItemAttributes[0].Title,
-                      description: results[rand3][rand1].ItemAttributes[0].Feature,
-                    },
-                    br2: {
-                      image2: results[rand4][rand2].LargeImage[0].URL,
-                      brand2: results[rand4][rand2].ItemAttributes[0].Brand,
-                      ProductType2: results[rand4][rand2].ItemAttributes[0].ProductTypeName,
-                      DetailPageURL2: results[rand4][rand2].DetailPageURL,
-                      pTitle2: results[rand4][rand2].ItemAttributes[0].Title,
-                      description: results[rand4][rand2].ItemAttributes[0].Feature
-                    }
+          if(result.length === 0 && pro1.type[0] === pro2.type[0] && pro1.title[0] !== pro2.title[0]) {
+            return knex
+              .insert({product_one: pro1.title[0], product_two: pro2.title[0]}).into('comparisons')
+              .then(function(result) {
+                // console.log(pro1.type);
+                // return result;
+                const productTitles = {
+                    pro1: results[rand3][rand1].ItemAttributes[0].Title,
+                    pro2: results[rand4][rand2].ItemAttributes[0].Title
+                  };
+                // console.log(results[0][rand1])
+                let templateVars = {
+                  br1: {
+                    image1: results[rand3][rand1].LargeImage[0].URL,
+                    brand1: results[rand3][rand1].ItemAttributes[0].Brand,
+                    ProductType1: results[rand3][rand1].ItemAttributes[0].ProductTypeName,
+                    DetailPageURL1: results[rand3][rand1].DetailPageURL,
+                    pTitle1: results[rand3][rand1].ItemAttributes[0].Title,
+                    description: results[rand3][rand1].ItemAttributes[0].Feature,
+                  },
+                  br2: {
+                    image2: results[rand4][rand2].LargeImage[0].URL,
+                    brand2: results[rand4][rand2].ItemAttributes[0].Brand,
+                    ProductType2: results[rand4][rand2].ItemAttributes[0].ProductTypeName,
+                    DetailPageURL2: results[rand4][rand2].DetailPageURL,
+                    pTitle2: results[rand4][rand2].ItemAttributes[0].Title,
+                    description: results[rand4][rand2].ItemAttributes[0].Feature
                   }
-              res.render("searchres", templateVars);
-            })
-            .catch(function(err) {
-              console.log(err);
-              res.status(500).render("error");
-            })
-          } else {
+                }
+                res.render("searchres", templateVars);
+              })
+          } else if (pro1.title[0] !== pro2.title[0]){
               const productTitles = {
                 pro1: results[rand3][rand1].ItemAttributes[0].Title,
                 pro2: results[rand4][rand2].ItemAttributes[0].Title
@@ -111,14 +110,14 @@ module.exports = (knex) => {
              }
             res.render("searchres", templateVars);
           }
-        }).catch(function(err) {
-          console.log(err);
-          res.status(500).render("error");
+
+          return null;
         })
         // return results;
     })
-    .catch(function(err){
-      console.log('ERROR', err);
+    .catch(function(err) {
+      console.log(err);
+      res.status(500).render("error");
     });
 
   });
