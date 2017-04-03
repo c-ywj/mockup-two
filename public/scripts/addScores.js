c750190aee391eac354995d7baee89bd3bb0f9fe
Lucy Wang <LucyTheGreat@Lucys-MacBook-Air.local>
2017-04-02 22:30:52 -0400

    alignment solved, made the results into a partial, now we need to finish stylize it

--- public/scripts/addScores.js
+++ public/scripts/addScores.js
@@ -5,9 +5,7 @@ $(() => {
 
     $('.voteBtn').on('click', function(ev) {
       $('.res').css('display','block');
-      $productCard.removeClass('s4 m5');
-      $productCard.addClass('s2 m3');
-      $resultsCard.addClass('s2 m3');
+
 
 
       $('#firstProd').css('-webkit-transform','rotateY(70deg) translateX( -150px )  translateY(24px) translateZ(50px)');
