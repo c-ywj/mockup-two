ff16426159bbf944d756b305edd0fe018f706abd
Florence <smc.florence@gmail.com>
2017-04-02 22:03:34 -0400

    results cards are now horizontally aligned with the products cards upon user's vote action

--- public/scripts/addScores.js
+++ public/scripts/addScores.js
@@ -1,8 +1,15 @@
 
 $(() => {
+  let $productCard = $('.card_products');
+  let $resultsCard = $('.card_results');
 
     $('.voteBtn').on('click', function(ev) {
       $('.res').css('display','block');
+      $productCard.removeClass('s4 m5');
+      $productCard.addClass('s2 m3');
+      $resultsCard.addClass('s2 m3');
+
+
       $('#firstProd').css('-webkit-transform','rotateY(70deg) translateX( -150px )  translateY(24px) translateZ(50px)');
       $('#secondProd').css('-webkit-transform','rotateY(-70deg) translateX(150px) translateY(24px) translateZ(50px)' );
       $('.voteBtn').hide();
