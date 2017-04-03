3357fe0834be3c85e22484f14747efb384ead65c
Florence <smc.florence@gmail.com>
2017-04-02 22:50:28 -0400

    adjusted column widths of products cards and results card for aesthetics, also vertically aligned results card to the top and centered it within its container

--- public/scripts/addScores.js
+++ public/scripts/addScores.js
@@ -5,14 +5,13 @@ $(() => {
 
     $('.voteBtn').on('click', function(ev) {
       $('.res').css('display','block');
-
-
+      $productCard.removeClass('s4 m5');
+      $productCard.addClass('s2 m3');
+      $resultsCard.addClass('s2 m3');
 
       $('#firstProd').css('-webkit-transform','rotateY(70deg) translateX( -150px )  translateY(24px) translateZ(50px)');
       $('#secondProd').css('-webkit-transform','rotateY(-70deg) translateX(150px) translateY(24px) translateZ(50px)' );
       $('.voteBtn').hide();
     })
-
-
 
 });
