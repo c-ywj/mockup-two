d273d54211fa10f23627a362f782dfe0b1ad0a55
Florence <smc.florence@gmail.com>
2017-04-04 19:36:28 -0400

    pushing favourites to localStorage fave list

--- public/scripts/addFavorites.js
+++ public/scripts/addFavorites.js
@@ -1,23 +1,48 @@
+'use strict';
+
 $(() => {
+  var $addFav = $('.add-fav');
+  var addToFave = localStorage['addFav'];
+  // this stores all saved favourites into an array
+  var currentFavList = localStorage['sessionFav'];
 
-  $('#heart1').on('click', function(ev) {
-    $('.fav1').hide();
-    $('.fav2').css('display','block');
-  })
+  $addFav.on('click', function(e) {
+    var $testItem = $(this).data('item');
 
-  $('#heart2').on('click', function(ev) {
-    $('.fav2').hide();
-    $('.fav1').css('display','block');
-  })
+    if (!addToFave) {
+      localStorage.setItem('addFav', $testItem);
+      localStorage.setItem('sessionFav', [addToFave]);
+    } else {
+      localStorage.setItem('addFav', (addToFave + `, ${$testItem}`));
+      localStorage.setItem('sessionFav', `[${addToFave}]`);
+    }
 
-  $('#heart3').on('click', function(ev) {
-    $('.fav3').hide();
-    $('.fav4').css('display','block');
-  })
+    $(this).html('<i class="material-icons saved-fave">favorite</i> Added to list');
 
-  $('#heart4').on('click', function(ev) {
-    $('.fav4').hide();
-    $('.fav3').css('display','block');
-  })
+    console.log('DATA-ATTR', $testItem);
+    console.log('addFav: ', addToFave, '\nsessionFave: ', currentFavList);
+    // console.log((e.currentTarget).attr('data-item'));
+
+  });
+
+  // $('#heart1').on('click', function(ev) {
+  //   $('.fav1').hide();
+  //   $('.fav2').css('display','block');
+  // })
+
+  // $('#heart2').on('click', function(ev) {
+  //   $('.fav2').hide();
+  //   $('.fav1').css('display','block');
+  // })
+
+  // $('#heart3').on('click', function(ev) {
+  //   $('.fav3').hide();
+  //   $('.fav4').css('display','block');
+  // })
+
+  // $('#heart4').on('click', function(ev) {
+  //   $('.fav4').hide();
+  //   $('.fav3').css('display','block');
+  // })
 
 });
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -30,4 +30,6 @@ $(() => {
   // Initialize collapse button
   $(".button-collapse").sideNav();
 
+  
+
 });
